69bd211f2a647c88be12c8dfb5c8a746b3e45f3d
yahavi <yahavi@jfrog.com>
2019-05-29 12:36:08 +0300

    Support Fixed Versions for issues

--- src/main/java/com/jfrog/ide/common/utils/ProjectsMap.java
+++ src/main/java/com/jfrog/ide/common/utils/ProjectsMap.java
@@ -9,6 +9,9 @@ import java.util.Comparator;
 import java.util.TreeMap;
 
 /**
+ * Maps project's key to project's dependencies tree. The project's key is a unique key based on the name and the path.
+ * Used in IDEs tree browsers to distinct between different projects.
+ *
  * @author yahavi
  */
 @SuppressWarnings("unused")
@@ -34,20 +37,27 @@ public class ProjectsMap extends TreeMap<ProjectsMap.ProjectKey, DependenciesTre
      * Comparable key. The keys are sorted by name and path.
      */
     public static class ProjectKey implements Comparable<ProjectKey> {
-        private String projectName;
-        private String projectPath;
+        String projectName;
+        String projectPath;
 
         ProjectKey(String projectName, String projectPath) {
-            this.projectName = projectName;
-            this.projectPath = projectPath;
+            this.projectName = StringUtils.defaultIfBlank(projectName, "");
+            this.projectPath = StringUtils.defaultIfBlank(projectPath, "");
         }
 
         @Override
         public int compareTo(@Nonnull ProjectKey other) {
-            return Comparator
-                    .comparing((ProjectsMap.ProjectKey key) -> StringUtils.compare(key.projectName, other.projectName))
-                    .thenComparing(key -> StringUtils.compare(key.projectPath, other.projectPath)).compare(this, other);
+            return Comparator.comparing(ProjectKey::getProjectName)
+                    .thenComparing(ProjectKey::getProjectPath)
+                    .compare(this, other);
         }
 
+        private String getProjectName() {
+            return projectName;
+        }
+
+        private String getProjectPath() {
+            return projectPath;
+        }
     }
 }
