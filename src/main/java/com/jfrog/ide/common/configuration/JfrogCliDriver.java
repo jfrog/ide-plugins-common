51ae9dff1b50fc0aa497babdd10a6b4e2d3d21d3
Tal Arian <tala@jfrog.com>
2021-11-16 18:19:36 +0200

    Add new JFrog CLI driver (#63)
    * Add new JFrog CLI driver
    
    Co-authored-by: Yahav Itzhak <yahavi@users.noreply.github.com>

---
+++ src/main/java/com/jfrog/ide/common/configuration/JfrogCliDriver.java
@@ -0,0 +1,86 @@
+package com.jfrog.ide.common.configuration;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.apache.commons.lang3.StringUtils;
+import org.jfrog.build.api.util.Log;
+import org.jfrog.build.extractor.executor.CommandExecutor;
+import org.jfrog.build.extractor.executor.CommandResults;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Paths;
+import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import static com.jfrog.ide.common.utils.Utils.createMapper;
+
+/**
+ * @author Tal Arian
+ */
+public class JfrogCliDriver {
+
+    private static final ObjectMapper jsonReader = createMapper();
+    private final CommandExecutor commandExecutor;
+
+    public JfrogCliDriver(Map<String, String> env) {
+        this(env, "");
+    }
+
+    public JfrogCliDriver(Map<String, String> env, String path) {
+        this.commandExecutor = new CommandExecutor(Paths.get(path, "jfrog").toString(), env);
+    }
+
+    @SuppressWarnings("unused")
+    public boolean isJfrogCliInstalled() {
+        try {
+            version(null);
+            return true;
+        } catch (IOException | InterruptedException e) {
+            return false;
+        }
+    }
+
+    public JfrogCliServerConfig getServerConfig() throws IOException {
+        return getServerConfig(Paths.get(".").toAbsolutePath().normalize().toFile(), Collections.emptyList());
+    }
+
+    public JfrogCliServerConfig getServerConfig(File workingDirectory, List<String> extraArgs) throws IOException {
+        List<String> args = new ArrayList<>();
+        args.add("config");
+        args.add("export");
+        args.addAll(extraArgs);
+        try {
+            CommandResults commandResults = commandExecutor.exeCommand(workingDirectory, args, null, null);
+            String res = commandResults.getRes();
+            if (StringUtils.isBlank(res) || !commandResults.isOk()) {
+                throw new IOException(commandResults.getErr());
+            }
+            // The output of the export command should be decoded before being parsed.
+            byte[] decodedBytes = Base64.getDecoder().decode(res.trim());
+            String decodedString = new String(decodedBytes);
+            return new JfrogCliServerConfig(jsonReader.readTree(decodedString));
+        } catch (IOException | InterruptedException e) {
+            throw new IOException("jfrog config export command failed." +
+                    "That might be happen if you haven't config any CLI server yet or using the config encryption feature.", e);
+        }
+    }
+
+    public String version(File workingDirectory) throws IOException, InterruptedException {
+        return runCommand(workingDirectory, new String[]{"--version"}, Collections.emptyList()).getRes();
+    }
+
+    private CommandResults runCommand(File workingDirectory, String[] args, List<String> extraArgs) throws IOException, InterruptedException {
+        return runCommand(workingDirectory, args, extraArgs, null);
+    }
+
+    public CommandResults runCommand(File workingDirectory, String[] args, List<String> extraArgs, Log logger) throws IOException, InterruptedException {
+        List<String> finalArgs = Stream.concat(Arrays.stream(args), extraArgs.stream()).collect(Collectors.toList());
+        CommandResults commandResults = commandExecutor.exeCommand(workingDirectory, finalArgs, null, logger);
+        if (!commandResults.isOk()) {
+            throw new IOException(commandResults.getErr() + commandResults.getRes());
+        }
+
+        return commandResults;
+    }
+}
---
+++ src/test/java/com/jfrog/ide/common/configuration/JfrogCliDriverTest.java
@@ -0,0 +1,104 @@
+package com.jfrog.ide.common.configuration;
+
+import com.google.common.collect.Lists;
+import org.apache.commons.lang3.SystemUtils;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.text.SimpleDateFormat;
+import java.util.*;
+
+import static org.testng.Assert.assertEquals;
+import static org.testng.Assert.fail;
+
+/**
+ * Test correctness of executing JFrog CLI commands
+ *
+ * @author tala
+ */
+public class JfrogCliDriverTest {
+
+    private final SimpleDateFormat timeStampFormat = new SimpleDateFormat("yyyy.MM.dd.HH.mm.ss");
+    private final Map<String, String> testEnv = new HashMap<>();
+    private JfrogCliDriver jfrogCliDriver;
+    private final String PASSWORD = "ide-plugins-common-test-password";
+    private final String USER_NAME = "ide-plugins-common-test-user";
+    private final String URL = "https://ide/plugins/common/test/";
+    private String testServerId;
+    private File tempDir;
+
+    @SuppressWarnings("unused")
+    @Test()
+    private void cliExportTest() {
+        try {
+            JfrogCliServerConfig serverConfig = jfrogCliDriver.getServerConfig(tempDir, Collections.emptyList());
+            assertEquals(serverConfig.getUsername(), USER_NAME);
+            assertEquals(serverConfig.getPassword(), PASSWORD);
+            assertEquals(serverConfig.getUrl(), URL);
+            assertEquals(serverConfig.getXrayUrl(), URL + "xray/");
+        } catch (IOException e) {
+            fail(e.getMessage(), e);
+        }
+    }
+
+    @BeforeMethod
+    public void setUp(Object[] testArgs) {
+        try {
+            configJfrogCli();
+            testServerId = createServerId();
+            String[] serverConfigCmdArgs = {"config", "add", testServerId, "--user=" + USER_NAME, "--password=" + PASSWORD, "--url=" + URL, "--interactive=false", "--enc-password=false"};
+            jfrogCliDriver.runCommand(tempDir, serverConfigCmdArgs, Collections.emptyList(), null);
+        } catch (IOException | InterruptedException e) {
+            fail(e.getMessage(), e);
+        }
+    }
+
+    private void configJfrogCli() {
+        try {
+            tempDir = Files.createTempDirectory("ide-plugins-common-cli-test").toFile();
+            tempDir.deleteOnExit();
+            getCli(tempDir);
+        } catch (IOException e) {
+            fail(e.getMessage(), e);
+        }
+        testEnv.put("JFROG_CLI_HOME_DIR", tempDir.getAbsolutePath());
+        jfrogCliDriver = new JfrogCliDriver(testEnv, tempDir.getAbsolutePath() + File.separator);
+
+    }
+
+    private void getCli(File execDir) throws IOException {
+        List<String> args;
+        if (SystemUtils.IS_OS_WINDOWS) {
+            args = Lists.newArrayList("cmd", "/c", "curl -XGET" +
+                    " \"https://releases.jfrog.io/artifactory/jfrog-cli/v2/[RELEASE]/jfrog-cli-windows-amd64/jfrog.exe\" -L -k -g");
+        } else {
+            args = new ArrayList<>() {{
+                add("/bin/sh");
+                add("-c");
+                add("curl -fL https://getcli.jfrog.io | bash -s v2");
+            }};
+        }
+        Process process = Runtime.getRuntime().exec(args.toArray(new String[0]), new String[0], execDir);
+        process.getOutputStream().close();
+    }
+
+
+    private String createServerId() {
+        return "ide-plugins-common-test-server-" + timeStampFormat.format(System.currentTimeMillis());
+    }
+
+    @AfterMethod
+    public void cleanUp() {
+        try {
+            String[] serverConfigCmdArgs = {"config", "remove", testServerId, "--quiet"};
+            jfrogCliDriver.runCommand(tempDir, serverConfigCmdArgs, Collections.emptyList(), null);
+        } catch (IOException | InterruptedException e) {
+            fail(e.getMessage(), e);
+        }
+    }
+
+}
