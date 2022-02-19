ce31b391bfd4a9c45d631d8fce12355339ef90f6
yahavi <yahavi@jfrog.com>
2022-02-19 11:51:52 +0200

    Allow scanning npm projects without node_modules

--- src/test/java/com/jfrog/ide/common/npm/NpmTreeBuilderTest.java
+++ src/test/java/com/jfrog/ide/common/npm/NpmTreeBuilderTest.java
@@ -3,10 +3,12 @@ package com.jfrog.ide.common.npm;
 import com.google.common.collect.Sets;
 import org.apache.commons.io.FileUtils;
 import org.jfrog.build.api.util.NullLog;
+import org.jfrog.build.client.Version;
 import org.jfrog.build.extractor.npm.NpmDriver;
 import org.jfrog.build.extractor.scan.DependencyTree;
 import org.jfrog.build.extractor.scan.GeneralInfo;
 import org.jfrog.build.extractor.scan.Scope;
+import org.testng.SkipException;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.DataProvider;
@@ -49,6 +51,8 @@ public class NpmTreeBuilderTest {
     }
 
     private final NpmDriver npmDriver = new NpmDriver(null);
+    private final boolean isNpm7 = isNpm7();
+    private DependencyTree dependencyTree;
     private File tempProject;
 
     @BeforeMethod
@@ -57,6 +61,13 @@ public class NpmTreeBuilderTest {
             tempProject = Files.createTempDirectory("ide-plugins-common-npm").toFile();
             tempProject.deleteOnExit();
             FileUtils.copyDirectory(((Project) testArgs[0]).path.toFile(), tempProject);
+
+            if ((Boolean) testArgs[1]) {
+                npmDriver.install(tempProject, Lists.newArrayList(), null);
+            }
+            NpmTreeBuilder npmTreeBuilder = new NpmTreeBuilder(tempProject.toPath(), null);
+            dependencyTree = npmTreeBuilder.buildTree(new NullLog(), false);
+            assertNotNull(dependencyTree);
         } catch (IOException e) {
             fail(e.getMessage(), e);
         }
@@ -68,7 +79,7 @@ public class NpmTreeBuilderTest {
     }
 
     @DataProvider
-    private Object[][] npmTreeBuilderProvider() {
+    private Object[][] npm6TreeBuilderProvider() {
         return new Object[][]{
                 {Project.EMPTY, false, 0},
                 {Project.EMPTY, true, 0},
@@ -81,34 +92,58 @@ public class NpmTreeBuilderTest {
         };
     }
 
+    @Test(dataProvider = "npm6TreeBuilderProvider")
+    public void npm6TreeBuilderTest(Project project, boolean install, int expectedChildren) {
+        if (isNpm7()) {
+            throw new SkipException("Skip test on npm >= 7");
+        }
+
+        String expectedProjectName = project.name;
+        if (!install && project.hasChildren) {
+            expectedProjectName += " (Not installed)";
+        }
+        checkDependencyTree(expectedProjectName, expectedChildren);
+    }
+
+    @DataProvider
+    private Object[][] npm7TreeBuilderProvider() {
+        return new Object[][]{
+                {Project.EMPTY, true, 0},
+                {Project.DEPENDENCY, false, 0},
+                {Project.DEPENDENCY, true, 2},
+                {Project.DEPENDENCY_PACKAGE_LOCK, false, 2},
+                {Project.DEPENDENCY_PACKAGE_LOCK, true, 2},
+                {Project.DEV_AND_PROD, false, 0},
+                {Project.DEV_AND_PROD, true, 1},
+        };
+    }
+
     @SuppressWarnings("unused")
-    @Test(dataProvider = "npmTreeBuilderProvider")
-    private void npmTreeBuilderTest(Project project, boolean install, int expectedChildren) {
-        try {
-            if (install) {
-                npmDriver.install(tempProject, Lists.newArrayList(), null);
-            }
-            NpmTreeBuilder npmTreeBuilder = new NpmTreeBuilder(tempProject.toPath(), null);
-            DependencyTree dependencyTree = npmTreeBuilder.buildTree(new NullLog(), false);
-            assertNotNull(dependencyTree);
-            String expectedProjectName = project.name;
-            if (!install && project.hasChildren) {
-                expectedProjectName += " (Not installed)";
-            }
-            checkGeneralInfo(dependencyTree.getGeneralInfo(), expectedProjectName, tempProject);
-            assertEquals(dependencyTree.getChildren().size(), expectedChildren);
-            switch (expectedChildren) {
-                case 0:
-                    noChildrenScenario(dependencyTree);
-                    break;
-                case 1:
-                    oneChildScenario(dependencyTree, expectedProjectName);
-                    break;
-                case 2:
-                    twoChildrenScenario(dependencyTree, expectedProjectName);
-            }
-        } catch (IOException e) {
-            fail(e.getMessage(), e);
+    @Test(dataProvider = "npm7TreeBuilderProvider")
+    public void npm7TreeBuilderTest(Project project, boolean install, int expectedChildren) {
+        if (!isNpm7()) {
+            throw new SkipException("Skip test on npm < 7");
+        }
+        String expectedProjectName = project.name;
+        boolean packageLockExist = Files.exists(tempProject.toPath().resolve("package-lock.json"));
+        if (!packageLockExist) {
+            expectedProjectName += " (Not installed)";
+        }
+        checkDependencyTree(expectedProjectName, expectedChildren);
+    }
+
+    private void checkDependencyTree(String expectedProjectName, int expectedChildren) {
+        checkGeneralInfo(dependencyTree.getGeneralInfo(), expectedProjectName, tempProject);
+        assertEquals(dependencyTree.getChildren().size(), expectedChildren);
+        switch (expectedChildren) {
+            case 0:
+                noChildrenScenario(dependencyTree);
+                break;
+            case 1:
+                oneChildScenario(dependencyTree, expectedProjectName);
+                break;
+            case 2:
+                twoChildrenScenario(dependencyTree, expectedProjectName);
         }
     }
 
@@ -128,7 +163,10 @@ public class NpmTreeBuilderTest {
     private void oneChildScenario(DependencyTree dependencyTree, String expectedProjectName) {
         DependencyTree child = dependencyTree.getChildren().get(0);
         assertEquals("progress:2.0.3", child.toString());
-        Set<Scope> expectedScopes = Sets.newHashSet(new Scope("prod"), new Scope("dev"));
+        Set<Scope> expectedScopes = Sets.newHashSet(new Scope("dev"));
+        if (!isNpm7) {
+            expectedScopes.add(new Scope("prod"));
+        }
         assertEquals(child.getScopes(), expectedScopes);
         assertEquals(child.getParent().toString(), expectedProjectName);
     }
@@ -146,6 +184,15 @@ public class NpmTreeBuilderTest {
                     fail("Unexpected dependency " + child);
             }
             assertEquals(child.getParent().toString(), expectedProjectName);
+        }
+    }
+
+    private boolean isNpm7() {
+        try {
+            Version version = new Version(npmDriver.version(new File(".")));
+            return version.isAtLeast(new Version("7.0.0"));
+        } catch (IOException | InterruptedException e) {
+            return false;
         }
     }
 }
