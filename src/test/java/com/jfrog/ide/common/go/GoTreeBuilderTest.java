74614c73b7b1c6dd10fe27841f5f4ba58279eaa6
yahavi <yahavi@jfrog.com>
2022-12-27 11:53:29 +0200

    Allow skip VCS stamping in Go

--- src/test/java/com/jfrog/ide/common/go/GoTreeBuilderTest.java
+++ src/test/java/com/jfrog/ide/common/go/GoTreeBuilderTest.java
@@ -1,6 +1,5 @@
 package com.jfrog.ide.common.go;
 
-import com.google.common.collect.Sets;
 import org.apache.commons.lang3.exception.ExceptionUtils;
 import org.jfrog.build.api.util.Log;
 import org.jfrog.build.api.util.NullLog;
@@ -13,9 +12,12 @@ import org.testng.annotations.Test;
 import java.io.IOException;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.*;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Vector;
 
-import static com.jfrog.ide.common.go.GoTreeBuilder.isBelow16;
+import static com.jfrog.ide.common.go.GoTreeBuilder.MIN_GO_VERSION;
+import static com.jfrog.ide.common.go.GoTreeBuilder.parseGoVersion;
 import static org.testng.Assert.*;
 
 /**
@@ -39,7 +41,7 @@ public class GoTreeBuilderTest {
         try {
             GoTreeBuilder treeBuilder = new GoTreeBuilder(null, GO_ROOT.resolve("project1"), null, log);
             DependencyTree dt = treeBuilder.buildTree();
-            validateDependencyTreeResults(expected, dt, true);
+            validateDependencyTreeResults(expected, dt);
         } catch (IOException ex) {
             fail(ExceptionUtils.getStackTrace(ex));
         }
@@ -56,7 +58,7 @@ public class GoTreeBuilderTest {
         try {
             GoTreeBuilder treeBuilder = new GoTreeBuilder(null, GO_ROOT.resolve("project2"), null, log);
             DependencyTree dt = treeBuilder.buildTree();
-            validateDependencyTreeResults(expected, dt, true);
+            validateDependencyTreeResults(expected, dt);
         } catch (IOException ex) {
             fail(ExceptionUtils.getStackTrace(ex));
         }
@@ -74,7 +76,7 @@ public class GoTreeBuilderTest {
         try {
             GoTreeBuilder treeBuilder = new GoTreeBuilder(null, GO_ROOT.resolve("project3"), null, log);
             DependencyTree dt = treeBuilder.buildTree();
-            validateDependencyTreeResults(expected, dt, true);
+            validateDependencyTreeResults(expected, dt);
         } catch (IOException ex) {
             fail(ExceptionUtils.getStackTrace(ex));
         }
@@ -92,80 +94,19 @@ public class GoTreeBuilderTest {
         try {
             GoTreeBuilder treeBuilder = new GoTreeBuilder(null, GO_ROOT.resolve("project4"), null, log);
             DependencyTree dt = treeBuilder.buildTree();
-            validateDependencyTreeResults(expected, dt, true);
+            validateDependencyTreeResults(expected, dt);
         } catch (IOException ex) {
             fail(ExceptionUtils.getStackTrace(ex));
         }
     }
 
-    private Map<String, List<String>> getAllDependenciesForTest() {
-        return new HashMap<>() {{
-            put("my/pkg/name1", Arrays.asList(
-                    "github.com/jfrog/directDep1@v0.1",
-                    "github.com/jfrog/directDep2@v0.2",
-                    "github.com/jfrog/directDep3@v0.3"));
-            put("github.com/jfrog/directDep1@v0.1", List.of(
-                    "github.com/jfrog/indirectDep1-1@v1.1",
-                    "github.com/jfrog/indirectDep2-1@v1.3"));
-            put("github.com/jfrog/directDep2@v0.2", Collections.singletonList(
-                    "github.com/jfrog/indirectDep1-2@v2.1"));
-            put("github.com/jfrog/indirectDep1-1@v1.1", Collections.singletonList(
-                    "github.com/jfrog/indirectIndirectDep1-1-1@v1.1.1"));
-        }};
-    }
-
-    @Test
-    public void testPopulateAllDependenciesMap() {
-        // Output of 'go mod graph'.
-        String[] dependenciesGraph = new String[]{
-                "my/pkg/name1 github.com/jfrog/directDep1@v0.1",
-                "my/pkg/name1 github.com/jfrog/directDep2@v0.2",
-                "my/pkg/name1 github.com/jfrog/directDep3@v0.3",
-                "github.com/jfrog/directDep1@v0.1 github.com/jfrog/indirectDep1-1@v1.1",
-                // github.com/jfrog/indirectDep2-1@v1.2 does not appear in the used modules set:
-                "github.com/jfrog/directDep1@v0.1 github.com/jfrog/indirectDep2-1@v1.2",
-                "github.com/jfrog/directDep1@v0.1 github.com/jfrog/indirectDep2-1@v1.3",
-                "github.com/jfrog/directDep2@v0.2 github.com/jfrog/indirectDep1-2@v2.1",
-                "github.com/jfrog/indirectDep1-1@v1.1 github.com/jfrog/indirectIndirectDep1-1-1@v1.1.1"
-        };
-        Set<String> usedModules = Sets.newHashSet("github.com/jfrog/directDep1@v0.1",
-                "github.com/jfrog/directDep2@v0.2",
-                "github.com/jfrog/directDep3@v0.3",
-                "github.com/jfrog/indirectDep1-1@v1.1",
-                "github.com/jfrog/indirectDep2-1@v1.3",
-                "github.com/jfrog/indirectDep1-2@v2.1",
-                "github.com/jfrog/indirectIndirectDep1-1-1@v1.1.1");
-        // Run method.
-        Map<String, List<String>> expectedAllDependencies = getAllDependenciesForTest();
-        Map<String, List<String>> actualAllDependencies = new HashMap<>();
-        GoTreeBuilder.populateAllDependenciesMap(dependenciesGraph, actualAllDependencies, usedModules);
-        // Validate result.
-        assertEquals(expectedAllDependencies, actualAllDependencies);
-    }
-
-    @Test
-    public void testPopulateDependencyTree() {
-        Map<String, Integer> expected = new HashMap<>() {{
-            put("github.com/jfrog/directDep1:0.1", 2);
-            put("github.com/jfrog/directDep2:0.2", 1);
-            put("github.com/jfrog/directDep3:0.3", 0);
-        }};
-        Map<String, List<String>> allDependencies = getAllDependenciesForTest();
-        DependencyTree rootNode = new DependencyTree();
-        GoTreeBuilder treeBuilder = new GoTreeBuilder(null, Paths.get(""), null, log);
-        treeBuilder.populateDependencyTree(rootNode, "my/pkg/name1", allDependencies);
-        validateDependencyTreeResults(expected, rootNode, false);
-    }
-
-    private void validateDependencyTreeResults(Map<String, Integer> expected, DependencyTree actual, boolean checkScope) {
+    private void validateDependencyTreeResults(Map<String, Integer> expected, DependencyTree actual) {
         Vector<DependencyTree> children = actual.getChildren();
         assertEquals(children.size(), expected.size());
         for (DependencyTree current : children) {
             assertTrue(expected.containsKey(current.toString()));
             assertEquals(current.getChildren().size(), expected.get(current.toString()).intValue());
-            if (checkScope) {
-                assertTrue(current.getScopes().contains(NONE_SCOPE));
-            }
+            assertTrue(current.getScopes().contains(NONE_SCOPE));
         }
     }
 
@@ -173,27 +114,22 @@ public class GoTreeBuilderTest {
     private Object[][] goVersionProvider() {
         return new Object[][]{
                 // Below 1.16
-                {"go version go1.9 darwin/amd64", true},
-                {"go version go1.15 darwin/amd64", true},
-                {"go version go1.15.4 darwin/amd64", true},
-
-                // Above 1.16
-                {"go version go1.16 darwin/amd64", false},
-                {"go version go1.16.1 darwin/amd64", false},
-                {"go version go1.17.7 darwin/amd64", false},
+                {"go version go1.9 darwin/amd64", "1.9"},
+                {"go version go1.15 darwin/amd64", "1.15"},
+                {"go version go1.15.4 darwin/amd64", "1.15.4"},
 
                 // Error values
-                {"go version 1.15 darwin/amd64", false},
-                {"go version 1.17 darwin/amd64", false},
-                {"1.17", false},
-                {"1.15", false},
+                {"go version 1.15 darwin/amd64", MIN_GO_VERSION.toString()},
+                {"go version 1.17 darwin/amd64", MIN_GO_VERSION.toString()},
+                {"1.17", MIN_GO_VERSION.toString()},
+                {"1.15", MIN_GO_VERSION.toString()},
         };
     }
 
     @Test(dataProvider = "goVersionProvider")
-    public void testIsBelow16(String versionOutput, boolean expectedBelow) {
+    public void testParseGoVersion(String versionOutput, String expectedVersion) {
         CommandResults commandResults = new CommandResults();
         commandResults.setRes(versionOutput);
-        assertEquals(expectedBelow, isBelow16(commandResults, new NullLog()));
+        assertEquals(expectedVersion, parseGoVersion(commandResults, new NullLog()).toString());
     }
 }
