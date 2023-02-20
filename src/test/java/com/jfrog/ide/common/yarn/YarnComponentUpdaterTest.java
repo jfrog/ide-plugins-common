bb97d942bb8bdd1e5e75e5518d4477968ecd1c90
Michael Sverdlov <michaelsv@jfrog.com>
2023-02-20 19:34:51 +0200

    comom

---
+++ src/test/java/com/jfrog/ide/common/yarn/YarnComponentUpdaterTest.java
@@ -0,0 +1,32 @@
+package com.jfrog.ide.common.yarn;
+
+import org.apache.commons.io.FileUtils;
+import org.apache.commons.lang3.exception.ExceptionUtils;
+import org.testng.annotations.Test;
+
+import java.io.File;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+import static org.testng.Assert.fail;
+
+/**
+ * Created by MichaelS
+ */
+public class YarnComponentUpdaterTest {
+    private static final Path GO_ROOT = Paths.get(".").toAbsolutePath().normalize().resolve(Paths.get("src", "test", "resources", "yarn"));
+
+    @Test
+    public void testYarnComponentUpdater() {
+        try {
+            File tempProject = Files.createTempDirectory("ide-plugins-common-go").toFile();
+            tempProject.deleteOnExit();
+            FileUtils.copyDirectory(GO_ROOT.resolve("dependency").toFile(), tempProject);
+            YarnComponentUpdater yarnComponentUpdater = new YarnComponentUpdater(tempProject.toPath(), null, null);
+            yarnComponentUpdater.run("@types/node", "14.14.11");
+        } catch (Exception ex) {
+            fail(ExceptionUtils.getStackTrace(ex));
+        }
+    }
+}
