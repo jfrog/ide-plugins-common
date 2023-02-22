6c467fca283fa0ab0463edb4cf7d80dd1fe32bed
Michael Sverdlov <michaelsv@jfrog.com>
2023-02-22 11:08:53 +0200

    comom

--- src/test/java/com/jfrog/ide/common/npm/NpmComponentUpdaterTest.java
+++ src/test/java/com/jfrog/ide/common/npm/NpmComponentUpdaterTest.java
@@ -15,14 +15,14 @@ import java.nio.file.Paths;
  * Created by MichaelS
  */
 public class NpmComponentUpdaterTest {
-    private static final Path GO_ROOT = Paths.get(".").toAbsolutePath().normalize().resolve(Paths.get("src", "test", "resources", "npm"));
+    private static final Path NPM_ROOT = Paths.get(".").toAbsolutePath().normalize().resolve(Paths.get("src", "test", "resources", "npm"));
     private static final Log log = new NullLog();
 
     @Test
     public void testNpmComponentUpdater() throws IOException {
         File tempProject = Files.createTempDirectory("ide-plugins-common-go").toFile();
         tempProject.deleteOnExit();
-        FileUtils.copyDirectory(GO_ROOT.resolve("dependency").toFile(), tempProject);
+        FileUtils.copyDirectory(NPM_ROOT.resolve("dependency").toFile(), tempProject);
         NpmComponentUpdater npmComponentUpdater = new NpmComponentUpdater(tempProject.toPath(), log, null);
         npmComponentUpdater.run("progress", "2.0.2");
     }
--- src/test/java/com/jfrog/ide/common/yarn/YarnComponentUpdaterTest.java
+++ src/test/java/com/jfrog/ide/common/yarn/YarnComponentUpdaterTest.java
@@ -13,13 +13,13 @@ import java.nio.file.Paths;
  * Created by MichaelS
  */
 public class YarnComponentUpdaterTest {
-    private static final Path GO_ROOT = Paths.get(".").toAbsolutePath().normalize().resolve(Paths.get("src", "test", "resources", "yarn"));
+    private static final Path YARN_ROOT = Paths.get(".").toAbsolutePath().normalize().resolve(Paths.get("src", "test", "resources", "yarn"));
 
     @Test
     public void testYarnComponentUpdater() throws IOException {
         File tempProject = Files.createTempDirectory("ide-plugins-common-go").toFile();
         tempProject.deleteOnExit();
-        FileUtils.copyDirectory(GO_ROOT.resolve("dependency").toFile(), tempProject);
+        FileUtils.copyDirectory(YARN_ROOT.resolve("dependency").toFile(), tempProject);
         YarnComponentUpdater yarnComponentUpdater = new YarnComponentUpdater(tempProject.toPath(), null, null);
         yarnComponentUpdater.run("@types/node", "14.14.11");
     }
