73fa04515fc6455c38d1d7699b615bf162d3284c
Tal Arian <tala@jfrog.com>
2023-06-11 15:39:50 +0300

    Add new FileIssueNode node

--- src/main/java/com/jfrog/ide/common/nodes/ApplicableIssueNode.java
+++ src/main/java/com/jfrog/ide/common/nodes/ApplicableIssueNode.java
@@ -1,27 +1,10 @@
 package com.jfrog.ide.common.nodes;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
-import com.jfrog.ide.common.nodes.subentities.Severity;
 
-public class ApplicableIssueNode extends IssueNode implements SubtitledTreeNode {
-    @JsonProperty()
-    private String name;
-    @JsonProperty()
-    private String reason;
-    @JsonProperty()
-    private String lineSnippet;
+public class ApplicableIssueNode extends FileIssueNode {
     @JsonProperty()
     private String scannerSearchTarget;
-    @JsonProperty()
-    private int rowStart;
-    @JsonProperty()
-    private int colStart;
-    @JsonProperty()
-    private int rowEnd;
-    @JsonProperty()
-    private int colEnd;
-    @JsonProperty()
-    private String filePath;
     @JsonProperty()
     private VulnerabilityNode issue;
 
@@ -31,73 +14,15 @@ public class ApplicableIssueNode extends IssueNode implements SubtitledTreeNode 
     }
 
     public ApplicableIssueNode(String name, int rowStart, int colStart, int rowEnd, int colEnd, String filePath, String reason, String lineSnippet, String scannerSearchTarget, VulnerabilityNode issue) {
-        this.name = name;
-        this.rowStart = rowStart;
-        this.colStart = colStart;
-        this.rowEnd = rowEnd;
-        this.colEnd = colEnd;
-        this.filePath = filePath;
-        this.reason = reason;
-        this.lineSnippet = lineSnippet;
+        super(name, filePath, rowStart, colStart, rowEnd, colEnd, reason, lineSnippet);
         this.scannerSearchTarget = scannerSearchTarget;
         this.issue = issue;
-    }
-
-    public Severity getSeverity() {
-        return issue.getSeverity();
-    }
-
-    @Override
-    public String getTitle() {
-        return name;
-    }
-
-    @Override
-    public String getSubtitle() {
-        return "row: " + (rowStart + 1) + " col: " + colStart;
-    }
-
-    @Override
-    public String getIcon() {
-        return getSeverity().getIconName();
     }
 
     public VulnerabilityNode getIssue() {
         return issue;
     }
 
-    @SuppressWarnings("unused")
-    public String getFilePath() {
-        return filePath;
-    }
-
-    @SuppressWarnings("unused")
-    public int getRowStart() {
-        return rowStart;
-    }
-
-    @SuppressWarnings("unused")
-    public int getColStart() {
-        return colStart;
-    }
-
-    @SuppressWarnings("unused")
-    public int getRowEnd() {
-        return rowEnd;
-    }
-
-    @SuppressWarnings("unused")
-    public int getColEnd() {
-        return colEnd;
-    }
-
-    public String getReason() {
-        return reason;
-    }
-
-    public String getLineSnippet() {
-        return lineSnippet;
-    }
 
     public String getScannerSearchTarget() {
         return scannerSearchTarget;
---
+++ src/main/java/com/jfrog/ide/common/nodes/FileIssueNode.java
@@ -0,0 +1,95 @@
+package com.jfrog.ide.common.nodes;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.jfrog.ide.common.nodes.subentities.Severity;
+
+public class FileIssueNode extends IssueNode implements SubtitledTreeNode {
+    @JsonProperty()
+    private String name;
+    @JsonProperty()
+    private String reason;
+    @JsonProperty()
+    private String lineSnippet;
+    @JsonProperty()
+    private int rowStart;
+    @JsonProperty()
+    private int colStart;
+    @JsonProperty()
+    private int rowEnd;
+    @JsonProperty()
+    private int colEnd;
+    @JsonProperty()
+    private String filePath;
+    @JsonProperty()
+    private Severity severity;
+
+    // Empty constructor for deserialization
+    @SuppressWarnings("unused")
+    protected FileIssueNode() {
+    }
+
+    public FileIssueNode(String name, String filePath, int rowStart, int colStart, int rowEnd, int colEnd, String reason, String lineSnippet) {
+        this.name = name;
+        this.filePath = filePath;
+        this.rowStart = rowStart;
+        this.colStart = colStart;
+        this.rowEnd = rowEnd;
+        this.colEnd = colEnd;
+        this.reason = reason;
+        this.lineSnippet = lineSnippet;
+    }
+
+    @SuppressWarnings("unused")
+    public String getFilePath() {
+        return filePath;
+    }
+
+    @SuppressWarnings("unused")
+    public int getRowStart() {
+        return rowStart;
+    }
+
+    @SuppressWarnings("unused")
+    public int getColStart() {
+        return colStart;
+    }
+
+    @SuppressWarnings("unused")
+    public int getRowEnd() {
+        return rowEnd;
+    }
+
+    @SuppressWarnings("unused")
+    public int getColEnd() {
+        return colEnd;
+    }
+
+    public String getReason() {
+        return reason;
+    }
+
+    public String getLineSnippet() {
+        return lineSnippet;
+    }
+
+    @Override
+    // The indexes ranges start form 0, for user readability convert the range to start from 1.
+    public String getSubtitle() {
+        return "row: " + (rowStart + 1) + " col: " + (colStart + 1);
+    }
+
+    @Override
+    public Severity getSeverity() {
+        return severity;
+    }
+
+    @Override
+    public String getTitle() {
+        return name;
+    }
+
+    @Override
+    public String getIcon() {
+        return getSeverity().getIconName();
+    }
+}
