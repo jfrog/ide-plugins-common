f983ea93d8e6919881223be005ec5a4690935095
Tal Arian <tala@jfrog.com>
2023-07-26 14:21:24 +0300

    Adds createSSLContext Utility

--- src/main/java/com/jfrog/ide/common/utils/ArtifactoryConnectionUtils.java
+++ src/main/java/com/jfrog/ide/common/utils/ArtifactoryConnectionUtils.java
@@ -1,8 +1,6 @@
 package com.jfrog.ide.common.utils;
 
 import com.jfrog.ide.common.configuration.ServerConfig;
-import org.apache.http.conn.ssl.TrustAllStrategy;
-import org.apache.http.ssl.SSLContextBuilder;
 import org.jfrog.build.api.util.Log;
 import org.jfrog.build.client.ProxyConfiguration;
 import org.jfrog.build.extractor.clientConfiguration.ArtifactoryManagerBuilder;
@@ -12,6 +10,8 @@ import java.security.KeyManagementException;
 import java.security.KeyStoreException;
 import java.security.NoSuchAlgorithmException;
 
+import static com.jfrog.ide.common.utils.Utils.createSSLContext;
+
 /**
  * Represents connection utils for Artifactory.
  *
@@ -20,9 +20,7 @@ import java.security.NoSuchAlgorithmException;
 public class ArtifactoryConnectionUtils {
 
     public static ArtifactoryManagerBuilder createArtifactoryManagerBuilder(ServerConfig serverConfig, Log logger) throws KeyStoreException, NoSuchAlgorithmException, KeyManagementException {
-        SSLContext sslContext = serverConfig.isInsecureTls() ?
-                SSLContextBuilder.create().loadTrustMaterial(TrustAllStrategy.INSTANCE).build() :
-                serverConfig.getSslContext();
+        SSLContext sslContext = createSSLContext(serverConfig);
         return createAnonymousAccessArtifactoryManagerBuilder(serverConfig.getArtifactoryUrl(), serverConfig.getProxyConfForTargetUrl(serverConfig.getArtifactoryUrl()), logger)
                 .setUsername(serverConfig.getUsername())
                 .setPassword(serverConfig.getPassword())
