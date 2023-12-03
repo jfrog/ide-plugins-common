f105e3706722a0830236b31e2a6bf8b7366a7e40
noys <noys@jfrog.com>
2023-12-03 14:54:16 +0200

    :art:

--- src/main/java/com/jfrog/ide/common/go/GoScanWorkspaceCreator.java
+++ src/main/java/com/jfrog/ide/common/go/GoScanWorkspaceCreator.java
@@ -27,6 +27,7 @@ public class GoScanWorkspaceCreator implements FileVisitor<Path> {
     private final Path sourceDir;
     private final Path targetDir;
     private final Log logger;
+    private static final String[] EXCLUDED_DIRS = new String[]{".git", ".idea", ".vscode"};
 
     public GoScanWorkspaceCreator(String executablePath, Path sourceDir, Path targetDir, Path goModAbsDir, Map<String, String> env, Log logger) {
         this.goDriver = new GoDriver(executablePath, env, goModAbsDir.toFile(), logger);
@@ -37,6 +38,10 @@ public class GoScanWorkspaceCreator implements FileVisitor<Path> {
 
     @Override
     public FileVisitResult preVisitDirectory(Path dir, BasicFileAttributes attrs) throws IOException {
+        // Skip excluded directories.
+        if (StringUtils.equalsAny(dir.getFileName().toString(), EXCLUDED_DIRS)) {
+            return FileVisitResult.SKIP_SUBTREE;
+        }
         // Skip subdirectories with go.mod files.
         // These directories are different Go projects and their go files should not be in the root project.
         if (!sourceDir.equals(dir)) {
@@ -70,10 +75,6 @@ public class GoScanWorkspaceCreator implements FileVisitor<Path> {
             Files.copy(file, targetGoMod);
             goDriver.runCmd("run . -goModPath=" + targetGoMod.toAbsolutePath() + " -wd=" + sourceDir.toAbsolutePath(), true);
             return FileVisitResult.CONTINUE;
-        }
-        // Skip .git, .idea and .vscode (deny list) directories.
-        if (StringUtils.containsAny(fileName, ".git", ".idea", ".vscode")) {
-            return FileVisitResult.SKIP_SUBTREE;
         }
         // Files other than go.mod and *.go files are not necessary to build the dependency tree of used Go packages.
         // Therefore, we just create an empty file with the same name so go:embed files won't cause a missing file error.
