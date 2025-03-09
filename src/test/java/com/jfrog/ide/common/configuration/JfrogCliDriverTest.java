202091d644015e927a9dd5b997abfe6b418eee31
Eyal Kapon <eyalk@jfrog.com>
2025-03-09 11:54:27 +0200

    get values from env variables

--- src/test/java/com/jfrog/ide/common/configuration/JfrogCliDriverTest.java
+++ src/test/java/com/jfrog/ide/common/configuration/JfrogCliDriverTest.java
@@ -30,9 +30,10 @@ public class JfrogCliDriverTest {
     private final SimpleDateFormat timeStampFormat = new SimpleDateFormat("yyyy.MM.dd.HH.mm.ss");
     private final Map<String, String> testEnv = new HashMap<>();
     private JfrogCliDriver jfrogCliDriver;
-    private final String PASSWORD = "ide-plugins-common-test-password";
-    private final String USER_NAME = "ide-plugins-common-test-user";
-    private final String SERVER_URL = "https://ide/plugins/common/test/";
+    private final String PASSWORD = System.getenv("JFROG_CLI_TEST_PASSWORD");
+    private final String USER_NAME = System.getenv("JFROG_CLI_TEST_USER");
+    private final String SERVER_URL = System.getenv("JFROG_CLI_TEST_URL");
+    private final String ACCESS_TOKEN = System.getenv("JFROG_CLI_TEST_ACCESS_TOKEN");
     private final String ARTIFACTORY_URL = SERVER_URL + "artifactory/";
     private final String XRAY_URL = SERVER_URL + "xray/";
     private String testServerId;
@@ -137,7 +138,7 @@ public class JfrogCliDriverTest {
     public void testAddCliServerConfig_withAccessToken() {
         String accessToken = UUID.randomUUID().toString();
         try {
-            jfrogCliDriver.addCliServerConfig(XRAY_URL, ARTIFACTORY_URL, testServerId, null, null, accessToken, tempDir);
+            jfrogCliDriver.addCliServerConfig(XRAY_URL, ARTIFACTORY_URL, testServerId, null, null, ACCESS_TOKEN, tempDir);
             JfrogCliServerConfig serverConfig = jfrogCliDriver.getServerConfig(tempDir, Collections.emptyList());
             assertNotNull(serverConfig);
             assertEquals(serverConfig.getAccessToken(), accessToken);
