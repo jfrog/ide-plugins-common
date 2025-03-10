d857d8f83d72416c28bc16e17537f231e0b3bc7e
Eyal Kapon <eyalk@jfrog.com>
2025-03-10 15:37:44 +0200

    debugging

--- src/test/java/com/jfrog/ide/common/configuration/JfrogCliDriverTest.java
+++ src/test/java/com/jfrog/ide/common/configuration/JfrogCliDriverTest.java
@@ -47,6 +47,8 @@ public class JfrogCliDriverTest {
             assertEquals(serverConfig.getUsername(), USER_NAME);
             assertEquals(serverConfig.getPassword(), PASSWORD);
             assertEquals(serverConfig.getUrl(), SERVER_URL);
+            System.out.println("srver url" +SERVER_URL);
+            System.out.println("xray url" + serverConfig.getXrayUrl());
             assertEquals(serverConfig.getXrayUrl(), SERVER_URL + "xray/");
         } catch (IOException e) {
             fail(e.getMessage(), e);
