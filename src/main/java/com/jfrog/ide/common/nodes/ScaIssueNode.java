c540a1a5163ff61a48b84a4a5c01cf6ca4cbca44
kerenr-jfrog <kerenr@jfrog.com>
2025-03-18 11:39:50 +0200

    removed isDirectDependency field in ScaIssueNode

--- src/main/java/com/jfrog/ide/common/nodes/ScaIssueNode.java
+++ src/main/java/com/jfrog/ide/common/nodes/ScaIssueNode.java
@@ -12,8 +12,8 @@ import java.util.List;
 public class ScaIssueNode extends FileIssueNode {
     private Applicability applicability;
     private List<List<ImpactPath>> impactPaths;
-    private boolean isDirectDependency;
     private String fixedVersions;
+    // TODO: add isDirectDependency indication
 
     // Empty constructor for deserialization
     @SuppressWarnings("unused")
@@ -24,27 +24,6 @@ public class ScaIssueNode extends FileIssueNode {
         super(title,  reason,  SourceCodeScanType.SCA,  severity,  ruleID, fullDescription);
         this.applicability = applicability;
         this.impactPaths = impactPaths;
-        this.isDirectDependency = isDirectDependency(impactPaths, ruleID);
         this.fixedVersions = fixedVersions;
-    }
-
-    /**
-     * Determines if the issue is a direct dependency.
-     *
-     * @param impactPaths  The impact paths of the violated rule.
-     * @param ruleId       The rule ID associated with the issue.
-     * @return true if the issue is a direct dependency, false otherwise.
-     */
-    private boolean isDirectDependency(List<List<ImpactPath>> impactPaths, String ruleId) {
-        String dependencyName = ruleId.split("_")[1];
-        String dependencyVersion = ruleId.split("_")[2];
-        ImpactPath directDependency = new ImpactPath(dependencyName, dependencyVersion);
-
-        for (List<ImpactPath> impactPathsList : impactPaths) {
-            if (impactPathsList.get(1).equals(directDependency)) {
-                return true; // TODO: validate that the first element is the root project
-            }
-        }
-        return false;
     }
 }
--- src/test/java/com/jfrog/ide/common/parse/SarifParserTest.java
+++ src/test/java/com/jfrog/ide/common/parse/SarifParserTest.java
@@ -1,11 +1,28 @@
 package com.jfrog.ide.common.parse;
 
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
 public class SarifParserTest {
+    private String sarifReport;
+
+
+    private String readSarifReportFromFile(String filePath) throws IOException {
+//        String jsonFilePath = "src/test/resources/parse/sca_no_jas.json";
+        ObjectMapper objectMapper = new ObjectMapper();
+        JsonNode jsonNode = objectMapper.readTree(new FileInputStream(filePath));
+        return jsonNode.toString();
+    }
 
     @Test
-    public void setUp() {
+    public void testParseInvalidSarifReport() throws IOException {
+        this.sarifReport = readSarifReportFromFile("src/test/resources/parse/sca_no_jas.json");
 
     }
 }
