75f95e47359197d40b7ef424684b9b8fbdcf6e63
Assaf Attias <49212512+attiasas@users.noreply.github.com>
2026-05-04 10:26:18 +0300

    Update dependencies (#187)

--- src/main/java/com/jfrog/ide/common/nodes/subentities/ImpactPath.java
+++ src/main/java/com/jfrog/ide/common/nodes/subentities/ImpactPath.java
@@ -1,5 +1,6 @@
 package com.jfrog.ide.common.nodes.subentities;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Getter;
 
@@ -7,6 +8,7 @@ import java.util.Objects;
 
 
 @Getter
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class ImpactPath {
     @JsonProperty()
     private String name;
